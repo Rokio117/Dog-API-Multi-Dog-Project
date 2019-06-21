0ea4d87bbef49ee7066f29d16321f3b0f5e9ae81
Nick Hillary <hillan02@gmail.com>
2019-06-20 19:03:54 -0500

    ready for initial submit

---
+++ index.js
@@ -0,0 +1,30 @@
+let userNum = 3
+function getDogPhotos() {
+  dogUrl = `https://dog.ceo/api/breeds/image/random/${userNum}`
+  console.log(dogUrl);
+  fetch(dogUrl)
+  .then(response => response.json())
+  .then(function(responseJson) {
+    console.log(responseJson)
+    displayDogs(responseJson)
+  }
+  )
+}
+function getNumberOfDoges() {
+  $('#submit-button').click(function(event){
+    event.preventDefault();
+    userNum = $('#dog-number').val();
+    getDogPhotos();
+  })
+} 
+function displayDogs(response) {
+  dogNum = 0
+  for (i = 0; i < userNum; i++) {
+    $('#dog-picture').prepend(
+      `<img src="${response.message[dogNum]}" class= "dog-images"></img>`
+    );
+    dogNum++
+  };
+  
+}
+$(getNumberOfDoges);
